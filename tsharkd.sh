c05a50ebbab49cad84d50a976f42265cab0e09c2
Your Name <you@example.com>
2024-04-06 16:01:37 +0000

    added some helpful features ---

--- tsharkd.sh
+++ tsharkd.sh
@@ -18,11 +18,50 @@ function clean_all(){
   done
 }
 
-
 function dry_run(){
         echo "cntl -c to stop... "
         echo ${TSHRKCMD} -i ${INTERFACES} -f "${FILTER}"
         ${TSHRKCMD} -i ${INTERFACES} -f "${FILTER}"
+}
+
+function help(){
+
+	cmd=`basename $0`
+
+	echo -ne "\n\n"
+	echo -ne " ${cmd}: \n"
+	echo -ne "\n"
+	echo -ne "\tThis script will run a rotating backgrounded tshark trace to capture \n"
+	echo -ne "\tnetwork events for later analysis.\n"
+	echo -ne "\t\n"
+	echo -ne "\ttarget commands:\n\n"
+	echo -ne "\thelp: this message\n\n"
+	echo -ne "\tstart: runs the current invocation in the backgroun\n\n"
+	echo -ne "\tstop:  stops the current invocation\n\n"
+	echo -ne "\trestart: runs the current invocation in the backgroun\n\n"
+	echo -ne "\tstatus: print some details about the running job\n\n"
+	echo -ne "\tdryrun: run the tshark command interactively as a test\n\n"
+	echo -ne "\tclean: clean the pcap files in the configured storage directory: $STORAGEDIR\n\n"
+	echo -ne "\t\n"
+	echo -ne "\t\n"
+	echo -ne "\t\n"
+	echo -ne "\t\n"
+
+}
+
+function get_status(){
+
+	if [[ ! -e "${PIDDIR}${PIDFILE}" ]]; then 
+		echo -e " Does not appear that there is a file: ${PIDDIR}${PIDFILE}"
+		return 
+	fi
+
+	p=`cat ${PIDDIR}${PIDFILE}`
+
+	for i in $p 
+	do 
+		echo $i 
+	done
 }
 
 
@@ -35,7 +74,7 @@ case $1 in
         for i in $INTERFACES
         do
                 echo $i
-                nohup ${TSHRKCMD} -i $i -b "duration:3600" -b "files:24" -w /opt/SnifferSnapShot-$i.pcap  -f "${FILTER}"  2>&1 &
+                nohup ${TSHRKCMD} -i $i -b "duration:3600" -b "files:24" -w /opt/SnifferSnapShot-${i}.pcap  -f "${FILTER}"  2>&1 &
                 procs=" $procs $!"
         done
         echo $procs > ${PIDDIR}${PIDFILE}
@@ -49,7 +88,6 @@ case $1 in
                 do
                         kill $i
                 done
-
                 ;;
 
         restart)
@@ -57,16 +95,21 @@ case $1 in
                 $0 start
                 ;;
 
-        dryrun)
+	status) 
+		get_status 
+		;;
 
+        dryrun)
                 dry_run
                 ;;
         clean)
                 clean_all
                 ;;
+	help)
+		help
+		;;
         *)
-
-                echo -e " $0 <start|stop|restart|status|dryrun|clean>"
+                echo -e " $0 <help|start|stop|restart|status|dryrun|clean>"
                 ;;
 
 esac
